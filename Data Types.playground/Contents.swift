277fcba9d63a9448057eee6262a2e1bd4427d8fe
Bob Tate <bob@Roberts-MacBook-Air.local>
2020-03-30 19:35:56 -0500

    completed lesson

--- Data Types.playground/Contents.swift
+++ Data Types.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var str = "Hello, playground"
+var thirdPlanet = "Earth"
+var ninthPlanet: String
+
+ninthPlanet = "Pluto"
+
+let numberOfCountriesInAfrica = 54
+
+let sixthPlanet = "Saturn"
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) orbiting \(sixthPlanet).")
+
